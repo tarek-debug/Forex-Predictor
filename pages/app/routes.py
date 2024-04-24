fd87062abf7028f537746e211a03f85f1d785e49
tarek-debug <tareksolamy111@gmail.com>
2024-04-23 22:55:51 -0400

    working on a new login

--- pages/app/routes.py
+++ pages/app/routes.py
@@ -8,3 +8,12 @@ def home():
     return render_template('login.html')
 
 # You can add more routes here
+@app.route('/login', methods=['POST'])
+def login():
+    # Your logic to verify username and password
+    pass
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Your logic to register new user and check for unique username and email
+    pass
