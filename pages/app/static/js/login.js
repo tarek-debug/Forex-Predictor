7c12560b1980d90cfbb1f353108a32f26bcb5cc9
tarek-debug <tareksolamy111@gmail.com>
2024-04-23 22:55:51 -0400

    working on a new login

--- pages/app/static/js/login.js
+++ pages/app/static/js/login.js
@@ -1,76 +1,60 @@
-const mode = 'login';
+function showRegistration() {
+  document.getElementById('loginForm').style.display = 'none';
+  document.getElementById('registrationForm').style.display = 'block';
+}
 
-class LoginComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mode: this.props.mode };
+function showLogin() {
+  document.getElementById('registrationForm').style.display = 'none';
+  document.getElementById('loginForm').style.display = 'block';
+}
 
+function login() {
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const data = { username, password };
+
+  fetch('/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Success:', data);
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
+}
+
+function register() {
+  const username = document.getElementById('regUsername').value;
+  const email = document.getElementById('regEmail').value;
+  const password = document.getElementById('regPassword').value;
+  const confirmPassword = document.getElementById('regConfirmPassword').value;
+
+  if (password !== confirmPassword) {
+      alert("Passwords do not match!");
+      return;
   }
-  toggleMode() {
-    var newMode = this.state.mode === 'login' ? 'signup' : 'login';
-    this.setState({ mode: newMode });
-  }
-  render() {
-    return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("div", { className: `form-block-wrapper form-block-wrapper--is-${this.state.mode}` }), /*#__PURE__*/
-      React.createElement("section", { className: `form-block form-block--is-${this.state.mode}` }, /*#__PURE__*/
-      React.createElement("header", { className: "form-block__header" }, /*#__PURE__*/
-      React.createElement("h1", null, this.state.mode === 'login' ? 'Welcome back!' : 'Sign up'), /*#__PURE__*/
-      React.createElement("div", { className: "form-block__toggle-block" }, /*#__PURE__*/
-      React.createElement("span", null, this.state.mode === 'login' ? 'Don\'t' : 'Already', " have an account? Click here \u2192"), /*#__PURE__*/
-      React.createElement("input", { id: "form-toggler", type: "checkbox", onClick: this.toggleMode.bind(this) }), /*#__PURE__*/
-      React.createElement("label", { htmlFor: "form-toggler" }))), /*#__PURE__*/
 
+  const data = { username, email, password };
 
-      React.createElement(LoginForm, { mode: this.state.mode, onSubmit: this.props.onSubmit }))));
-
-
-
-  }}
-
-
-class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return /*#__PURE__*/(
-      React.createElement("form", { onSubmit: this.props.onSubmit }, /*#__PURE__*/
-      React.createElement("div", { className: "form-block__input-wrapper" }, /*#__PURE__*/
-      React.createElement("div", { className: "form-group form-group--login" }, /*#__PURE__*/
-      React.createElement(Input, { type: "text", id: "username", label: "user name", disabled: this.props.mode === 'signup' }), /*#__PURE__*/
-      React.createElement(Input, { type: "password", id: "password", label: "password", disabled: this.props.mode === 'signup' })), /*#__PURE__*/
-
-      React.createElement("div", { className: "form-group form-group--signup" }, /*#__PURE__*/
-      React.createElement(Input, { type: "text", id: "fullname", label: "full name", disabled: this.props.mode === 'login' }), /*#__PURE__*/
-      React.createElement(Input, { type: "email", id: "email", label: "email", disabled: this.props.mode === 'login' }), /*#__PURE__*/
-      React.createElement(Input, { type: "password", id: "createpassword", label: "password", disabled: this.props.mode === 'login' }), /*#__PURE__*/
-      React.createElement(Input, { type: "password", id: "repeatpassword", label: "repeat password", disabled: this.props.mode === 'login' }))), /*#__PURE__*/
-
-
-      React.createElement("button", { className: "button button--primary full-width", type: "submit" }, this.props.mode === 'login' ? 'Log In' : 'Sign Up')));
-
-
-  }}
-
-
-const Input = ({ id, type, label, disabled }) => /*#__PURE__*/
-React.createElement("input", { className: "form-group__input", type: type, id: id, placeholder: label, disabled: disabled });
-
-
-const App = () => /*#__PURE__*/
-React.createElement("div", { className: `app app--is-${mode}` }, /*#__PURE__*/
-React.createElement(LoginComponent, {
-  mode: mode,
-  onSubmit:
-  function () {
-    console.log('submit');
-  } }));
-
-
-
-
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById("app"));
+  fetch('/register', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Registration Success:', data);
+      showLogin(); // Automatically switch back to login form on successful registration
+  })
+  .catch((error) => {
+      console.error('Error:', error);
+  });
+}
