d07ee508118a6c8c7897832d83c21a3672fa7eca
tarek-debug <tareksolamy111@gmail.com>
2024-04-27 16:39:21 -0400

    Deleted unnecessary files and folders

--- generate_keys_for_env.py
+++
@@ -1,3 +0,0 @@
-import secrets
-print(secrets.token_urlsafe(16))  # Generates a secure token. You can use this for SECRET_KEY.
-print(secrets.token_urlsafe(16))  # Run again for a different value. Use this for CSRF_SECRET_KEY.
