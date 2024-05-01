9f5ff9ed6fdd730b448bf19a8b60785421fd45a9
HamimMahdie <144726390+HamimMahdie@users.noreply.github.com>
2024-05-01 17:59:57 -0400

    Update routes.py

--- pages/app/routes.py
+++ pages/app/routes.py
@@ -26,11 +26,12 @@ def login():
             return jsonify({'error': 'Login failed'}), response.status_code
     return render_template('login.html')
 
+
 @app.route('/logout')
 def logout():
     session.pop('username', None)
-    return redirect(url_for('login'))
-
+    return redirect(url_for('login.html'))
+    
 @app.route('/register', methods=['POST'])
 def register():
     user_data = request.json
